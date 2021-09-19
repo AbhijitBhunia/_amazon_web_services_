7b450a73038e1b49ffeafd54f3c1604a00b6d29d
bloodborne <vijayrmourya@gmail.com>
2021-09-19 16:19:50 +0530

    updated lambda

--- lambda_library_boto3/terraform-boto3/ec2-state-change-notify-lambda-tf.py
+++ lambda_library_boto3/terraform-boto3/ec2-state-change-notify-lambda-tf.py
@@ -1,7 +1,6 @@
-# lambda to send notification over mail extract EC2 details to be used with Terraform
 import boto3
 
-region = "${var.aws_region}"
+region = "ap-south-1"
 def lambda_handler(event, context):
     instance_id = event['detail']['instance-id']
     state = event['detail']["state"]
@@ -14,29 +13,37 @@ def lambda_handler(event, context):
     AMI_ID = myinstance['Reservations'][0]['Instances'][0]['ImageId']
     Instance_type = myinstance['Reservations'][0]['Instances'][0]['InstanceType']
     tags = myinstance['Reservations'][0]['Instances'][0]['Tags']
+    instance_name='No name tagged'
+
     TAG = ''
     for i in tags:
-        TAG=TAG+str(i)+'\n'
-    MY_SNS_TOPIC_ARN = "${aws_sns_topic.ec2_state_change_sns.arn}"
+        k=list(i.keys())
+        for j in k:
+            if str(i[j])=='name':
+                name = 'Tagged '+ i[j] +" : "+i['Value']
+            TAG = TAG + i[j] + ' '
+        TAG = TAG+'\n'
+
+    MY_SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:064558089886:state_change_sns_notify"
     sns_client = boto3.client('sns', region)
-    msg = 'Instance ID:   ' + instance_id + '\n' + 'State:   ' + state + '\n' + 'Region:   ' + region + '\n' + 'Instance AMI ID:   ' + AMI_ID + '\n' + 'Instance type:   ' + Instance_type + '\n' + 'Instance ARN:   ' + resources_ARN + '\n' + 'Tag details: \n' + TAG
+    msg = 'Instance ID:   ' + instance_id + '\n' + 'State:   ' + state + '\n' + 'Region:   ' + region + '\n' + 'Instance AMI ID:   ' + AMI_ID + '\n' + 'Instance type:   ' + Instance_type + '\n' + 'Instance ARN:   ' + resources_ARN + '\n' + 'Tag details as mentioned below: \n' + TAG
 
     if (state == "running"):
-        sub = instance_id + ' : EC2 Instance State changed to Running'
+        sub = 'EC2 Instance State changed to Running (' + name + ')'
         sns_client.publish(
             TopicArn = MY_SNS_TOPIC_ARN,
             Subject = sub,
             Message = msg
         )
     elif (state == "stopped"):
-        sub = instance_id + ' : EC2 Instance is stopped'
+        sub = 'EC2 Instance is stopped (' + name + ')'
         sns_client.publish(
             TopicArn = MY_SNS_TOPIC_ARN,
             Subject = sub,
             Message = msg
         )
     elif (state == "terminated"):
-        sub = instance_id + ' : EC2 Instance is Terminated'
+        sub = 'EC2 Instance is Terminated (' + name + ')'
         sns_client.publish(
             TopicArn = MY_SNS_TOPIC_ARN,
             Subject = sub,
