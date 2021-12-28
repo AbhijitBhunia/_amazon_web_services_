4b2504eb5f627029abb5feb12242cc68cad8d5db
bloodborne <vijayrmourya@gmail.com>
2021-12-28 21:52:54 +0530

    create-ec2-spot-instance-request project updated

--- terraform-aws/create-ec2-spot-instance-request/f1-ec2-key-pair.tf
+++ terraform-aws/create-ec2-spot-instance-request/f1-ec2-key-pair.tf
@@ -1,14 +1,14 @@
-resource "tls_private_key" "this" {
+resource "tls_private_key" "tls-key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
 resource "aws_key_pair" "ec2-key-pair" {
   key_name   = var.instance_keypair
-  public_key = tls_private_key.this.public_key_openssh
+  public_key = tls_private_key.tls-key.public_key_openssh
 }
 
 resource "local_file" "private_key" {
-  content  = tls_private_key.this.private_key_pem
-  filename = "${path.module}/privet-key/create_ec2_in_every_AZ_in_region_key.pem"
+  content  = tls_private_key.tls-key.private_key_pem
+  filename = "${path.module}/private-key/create-ec2-spot-instance-request-key.pem"
 }
